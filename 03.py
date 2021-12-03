a1b2e2d7e852fb945ea3277af12a5d2f8ae0deb7
Wim Thys <wim.thys@zardof.be>
2021-12-03 15:54:46 +0100

    day3 solution

---
+++ 03.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import Counter
+
+
+def read_input(filename):
+    with open(filename) as content:
+        return [line.strip() for line in content]
+
+
+def most_common(bits):
+    mc = Counter(bits).most_common()
+
+    if len(mc) == 1:
+        return mc[0][0]
+
+    most, least = mc
+
+    if most[1] == least[1]:
+        return "1"
+
+    return most[0]
+
+
+def gamma_epsilon(report):
+    gamma = []
+    epsilon = []
+
+    epsmap = { "0" : "1", "1" : "0" }
+    
+    #unzip the zipped list
+    series = zip(*report)
+    for bits in series:
+        comm = most_common(bits)
+        gamma.append(comm)
+        epsilon.append(epsmap[comm])
+
+    return "".join(gamma), "".join(epsilon)
+
+
+def extract_rating(report, selector):
+    if len(report) == 0:
+        raise ValueError('no ratings available')
+
+    if len(report) == 1:
+        return report[0]
+    
+    gamma, epsilon = gamma_epsilon(report)
+
+    common_bit = selector(gamma, epsilon)[0]
+
+    return common_bit + extract_rating( [x[1:] for x in report if x[0] == common_bit], selector )
+
+
+def part_one(report):
+    gamma, epsilon = gamma_epsilon(report)
+
+    return int(gamma, 2) * int(epsilon, 2)
+
+
+def part_two(report):
+    oxy = int(extract_rating(report, lambda g, e: g), 2)
+    co2 = int(extract_rating(report, lambda g, e: e), 2)
+
+    print(f"oxy: {oxy:b}/{oxy:d}, co2: {co2:b}/{co2:d}")
+    return oxy * co2
+
+
+def main(filename):
+    report = read_input(filename)
+
+    print(f"part1: {part_one(report)}")
+    print(f"part2: {part_two(report)}")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
