240bbf2d644fe0da65ca8e038db81cc2fdf0f8d9
Wim Thys <wim.thys@zardof.be>
2021-12-11 15:13:56 +0100

    moved runner script into image so everything runs in the container

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM python:3.10-slim
 
 WORKDIR /app
 
-COPY requirements.txt /app/
+COPY requirements.txt runner.sh /app/
 
 RUN pip install -r requirements.txt
 
-COPY *.py *-examples.txt /app/
+COPY *.py *-example.txt /app/
+
+ENTRYPOINT ["/usr/bin/env", "bash", "runner.sh"]
--- runner.sh
+++ runner.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 
+SCRIPTDIR="$(dirname "$(readlink -f "$0")")"
+
 YEAR=2021
 
 DAY="$(echo 0$1 | sed 's/.*\(..\)$/\1/')"
 
 INPUT="$2"
 
-
-RUNDOCKER="docker run --rm -i aoc python $DAY.py"
-
-
+RUNDAY="python3 $DAY.py"
 
 case $INPUT in
     input)
         TMPFILE="$(mktemp)"
         aocd $DAY $YEAR > $TMPFILE || (rm -f $TMPFILE && exit 1)
 
-        cat $TMPFILE | $RUNDOCKER
+        $RUNDAY $TMPFILE
         rm -f $TMPFILE
         ;;
     *)
-        cat $DAY-example.txt | $RUNDOCKER
+        $RUNDAY $SCRIPTDIR/$DAY-example.txt
         ;;
 esac
