27c5f08aa4134a0bef818769356fa2b2282691f2
Wim Thys <wim.thys@zardof.be>
2021-12-12 12:35:14 +0100

    made build-image.sh more constrained to this year

--- build-image.sh
+++ build-image.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 LATEST_DAY="$(ls [012][0-9].py | sort | tail -1 | sed 's/\.py$//')"
 LATEST_YEAR="2021"
+TARGET="aoc-$LATEST_YEAR"
 
 if [[ -z "$1" ]]; then
-    docker build  -t aoc:latest -t aoc:$LATEST_YEAR-$LATEST_DAY . > /dev/null
+    docker build  -t $TARGET:latest -t $TARGET:$LATEST_DAY . > /dev/null
 else
-    docker build  -t aoc:latest -t aoc:$LATEST_YEAR-$LATEST_DAY .
+    docker build  -t $TARGET:latest -t $TARGET:$LATEST_DAY .
 fi
