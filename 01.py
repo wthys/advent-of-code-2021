49d7d0c66d924fc979561ac9483f213ca4457a8a
Wim Thys <wim.thys@zardof.be>
2021-12-16 17:11:43 +0100

    cleanup on day 1 now that we can use Python 3.10

--- 01.py
+++ 01.py
@@ -1,43 +1,38 @@
 #!/usr/bin/env python3
 
-from common import read_input
+from common import read_input, combine, clean
+from itertools import pairwise
 
 
 def differences(measurements):
-    diffs = []
-    for i, depth in enumerate(measurements):
-        if i > 0:
-            diffs.append(depth - measurements[i - 1])
-    return diffs
-
-def n_measurement(measurements, n=1):
-    if n < 1:
-        raise ValueError("n must be at least 1")
-    if int(n) != n:
-        raise ValueError("n must be int")
-    if n >= len(measurements):
-        return measurements
-
-    return [measurements[i:i+n] for i in range(len(measurements[:-n+1]))]
+    return list( b - a for a, b in pairwise(measurements) )
 
 
-def diff_n(measurements, n=1):
-    diffs = []
-    slices = [sum(grp) for grp in n_measurement(measurements, n)]
-    for i, total in enumerate(slices):
-        if i > 0:
-            diffs.append(total-slices[i-1])
-    return diffs
+def triple_measures(measurements):
+    return list( a + b + c for (a, b), (_, c) in pairwise(pairwise(measurements)) )
 
 
 def count_increases(measurements):
     return len([x for x in measurements if x > 0])
 
 
+def part_one(measurements):
+    diffs = differences(measurements)
+
+    return count_increases(diffs)
+
+
+def part_two(measurements):
+    triples = triple_measures(measurements)
+
+    diffs = differences(triples)
+
+    return count_increases(diffs)
+
 def main():
-    measurements = read_input(int)
-    print(f"part1: {count_increases(differences(measurements))}")
-    print(f"part2: {count_increases(diff_n(measurements, 3))}")
+    measurements = read_input(combine(int, clean))
+    print(f"part 1: {part_one(measurements)}")
+    print(f"part 2: {part_two(measurements)}")
 
 
 if __name__ == "__main__":
