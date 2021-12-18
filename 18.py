18978554c8a53f55f67e93320be7eb7315fc1d97
Wim Thys <wim.thys@zardof.be>
2021-12-18 23:12:53 +0100

    day 18 part 1

--- 18.py
+++ 18.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 
-from common import read_input, combine, clean, debug
+from common import read_input, combine, clean, debug, color
 
 import pyparsing as pp
 
-from collections import namedtuple
+from collections import namedtuple, defaultdict
+from copy import deepcopy
 
 
 def as_int(t):
@@ -31,141 +32,218 @@ pair = (OPEN
 sfn << ( single | pair ).set_results_name('num')
 
 
+def sfn_split(number, done = None):
+    number = deepcopy(number)
+    if done is None:
+        done = [False]
+
+    if done[0]:
+        return number
+
+
+    def descend(num):
+        if done[0]:
+            return num
+
+        match num:
+            case [a, b]:
+                return [descend(a), descend(b)]
+            case _:
+                if num >= 10:
+                    done[0] = True
+                    return [num // 2, num - num // 2]
+                return num
+
+    number = descend(number)
+    if done[0]:
+        pass#debug(f"--   split: {number}")
+    return number
+
+
+def sfn_explode(number, done = None):
+    number = deepcopy(number)
+
+    if done is None:
+        done = [False]
+
+    if done[0]:
+        return number
+
+    def find_fuse(num, trace = None):
+        if trace is None:
+            trace = []
+
+        match num:
+            case [a, b]:
+                if isinstance(a, int) and isinstance(b, int):
+                    if len(trace) >= 4:
+                        return trace
+                    else:
+                        return []
+
+                trc = find_fuse(num[0], trace + [0])
+                if len(trc) > 0:
+                    return trc
+                return find_fuse(num[1], trace + [1])
+            case _:
+                return []
+
+    trace = find_fuse(number)
+    if len(trace) == 0:
+        return number
+
+    done[0] = True
+
+    def pair_at(trace):
+        pair = number
+        for idx in trace:
+            pair = pair[idx]
+        return pair
+
+    left, right = pair_at(trace)
+    parent = pair_at(trace[:-1])
+
+    parent[trace[-1]] = 0
+
+    def find_prev(trace):
+        # find first parent with a left
+        trc = trace[:]
+        while trc.pop() == 0:
+            if len(trc) == 0:
+                return False
+        trc.append(0)
+
+        while True:
+            pair = pair_at(trc)
+            match pair:
+                case [_, a]:
+                    trc.append(1)
+                case _:
+                    return trc
+
+    def find_next(trace):
+        # find first parent with a right
+        trc = trace[:]
+        while trc.pop() == 1:
+            if len(trc) == 0:
+                return False
+        trc.append(1)
+
+        while True:
+            pair = pair_at(trc)
+            match pair:
+                case [a, _]:
+                    trc.append(0)
+                case _:
+                    return trc
+
+    prv = find_prev(trace)
+    if prv:
+        #debug(f"found prev @ {prv}")
+        pair = pair_at(prv[:-1])
+        #debug(f"before change @ {prv}: {pair} // {number}")
+        pair[prv[-1]] += left
+        #debug(f"after change  @ {prv}: {pair} // {number}")
+
+    nxt = find_next(trace)
+    if nxt:
+        #debug(f"found next @ {nxt}")
+        pair = pair_at(nxt[:-1])
+        #debug(f"before change @ {nxt}: {pair} // {number}")
+        pair[nxt[-1]] += right
+        #debug(f"after change  @ {nxt}: {pair} // {number}")
+
+    #debug(f"-- explode: {number}")
+    return number
+        
+
+def sfn_add(a, b):
+    number = [deepcopy(a), deepcopy(b)]
+
+    done = [True]
+    while done[0]:
+        done[0] = False
+        number = sfn_explode(number, done)
+        number = sfn_split(number, done)
+
+    return number
+
+
+def sfn_mag(number):
+    match number:
+        case [a, b]:
+            return 3 * sfn_mag(a) + 2 * sfn_mag(b)
+        case _:
+            return number
+
 
 def parse_number(line):
     return sfn.parseString(line)[0]
 
 
-def sfn_equals(a, b):
-    if type(a) != type(b):
-        return False
 
-    match a:
-        case [_, _]:
-            return sfn_equals(a[0], b[0]) and sfn_equals(a[1], b[1])
-
-        case _:
-            return a == b
-
-
-def sfn_split(number, done = None):
-    if done is None:
-        done = [False]
-
-    def mark_done():
-        done[0] = True
-
-    if done[0]:
-        return number
-
-    match number:
-        case [a, b]:
-            return  [sfn_split(a, done), sfn_split(b, done)]
-        case n:
-            if n < 10:
-                return n
-            mark_done()
-            return [n // 2, n - n // 2]
-
-
-def should_explode(num, trace = None):
-    if trace is None:
-        trace = []
-
-    match number:
-        case [a, b]:
-            if len(trace) >= 4:
-                return False
-
-            exa = should_explode(a, trace + [0])
-            if exa:
-                return exa
-
-            return should_explode(b, trace + [1])
-        case n:
-            if len(trace) == 4:
-                return trace
-            return False
-
-
-def sfn_explode(number, done = None):
-    if done is None:
-        done = [False]
-
-    def mark_done():
-        done[0] = True
-
-    if done or len(number) == 1:
-        return number
-
-    trace = should_explode(number)
-    if trace:
-        mark_done()
-        a, b, c, d = trace
-        parent = number[a][b][c]
-        pleft, pright = parent
-        eleft, eright = parent[d]
-        if d == 0:
-            number[a][b][c] = [0, eright + pright]
-        else:
-            number[a][b][c] = [eleft + pleft, 0]
-        return number
-
-    a, b = number
-    aa = sfn_explode(a)
-    if not sfn_equals(a, aa):
-        return [aa, b]
-
-    return [a, sfn_explode(b)]
-
-
-def sfn_reduce(number):
-    debug(f"original:      {number}")
-    while True:
-        reduced = sfn_explode(number)
-        debug(f"after explode: {reduced}")
-
-        if sfn_equals(number, reduced):
-            reduced = sfn_split(number)
-            debug(f"after split:   {reduced}")
-
-            if sfn_equals(number, reduced):
-                return number
-
-        number = reduced
-
-def sfn_test(expected, actual, message):
-    if sfn_equals(expected, actual):
-        return
-    print(f"{message} should return {expected}, got {actual}")
-
-
-test_case = namedtuple('test_case', ['func', 'input', 'expected'])
+test_case = namedtuple('test_case', ['func', 'args', 'expected'])
 
 def tests():
+
+    fails = [0]
+
+    def sfn_test(expected, actual, message):
+        if expected == actual:
+            return
+        fails[0] += 1
+        print(f"{message} should return {expected}, got {actual}")
+
     cases = [
-            test_case(sfn_explode, [[[[[9,8],1],2],3],4], [[[[0,9],2],3],4] ),
-            test_case(sfn_explode, [7,[6,[5,[4,[3,2]]]]], [7,[6,[5,[7,0]]]] ),
-            test_case(sfn_explode, [[6,[5,[4,[3,2]]]],1], [[6,[5,[7,0]]],3] ),
-            test_case(sfn_explode, [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]], [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]),
-            test_case(sfn_explode, [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]], [[3,[2,[8,0]]],[9,[5,[7,0]]]])
-            test_case(sfn_split, 10, [5,5] ),
-            test_case(sfn_split, 11, [5,6] ),
-            test_case(sfn_split, 18, [9,9] ),
+            test_case(sfn_explode, ([[[[[9,8],1],2],3],4],), [[[[0,9],2],3],4] ),
+            test_case(sfn_explode, ([7,[6,[5,[4,[3,2]]]]],), [7,[6,[5,[7,0]]]] ),
+            test_case(sfn_explode, ([[6,[5,[4,[3,2]]]],1],), [[6,[5,[7,0]]],3] ),
+            test_case(sfn_explode, ([[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]],), [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]),
+            test_case(sfn_explode, ([[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]],), [[3,[2,[8,0]]],[9,[5,[7,0]]]]),
+            test_case(sfn_split, (10,), [5,5] ),
+            test_case(sfn_split, (11,), [5,6] ),
+            test_case(sfn_split, (18,), [9,9] ),
+            test_case(sfn_add, ([[[[4,3],4],4],[7,[[8,4],9]]], [1,1]), [[[[0,7],4],[[7,8],[6,0]]],[8,1]] ),
+            test_case(sfn_mag, ([[1,2],[[3,4],5]],), 143),
+            test_case(sfn_mag, ([[[[0,7],4],[[7,8],[6,0]]],[8,1]],), 1384),
+            test_case(sfn_mag, ([[[[1,1],[2,2]],[3,3]],[4,4]],), 445),
+            test_case(sfn_mag, ([[[[3,0],[5,3]],[4,4]],[5,5]],), 791),
+            test_case(sfn_mag, ([[[[5,0],[7,4]],[5,5]],[6,6]],), 1137),
+            test_case(sfn_mag, ([[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]],), 3488),
             ]
 
     for cs in cases:
-        sfn_test(cs.expected, cs.func(cs.input), f"{cs.func.__name__}({cs.input})")
+        sfn_test(cs.expected, cs.func(*(cs.args)), f"{cs.func.__name__}({', '.join(map(str,cs.args))})")
+    
+    return fails[0]
+
 
 
 def main():
     numbers = read_input(combine(parse_number, clean))
 
-    tests()
+    if tests() > 0:
+        return
 
-    print(sfn_reduce([[[[[4,3],4],4],[7,[[8,4],9]]], [1,1]]))
+    print(f"part 1: {part_one(numbers[:])}")
+    print(f"part 2: {part_two(numbers[:])}")
 
-    
+
+def part_one(numbers):
+
+    total = numbers[0]
+    for number in numbers[1:]:
+        debug(" ")
+        debug(f"  {total}")
+        debug(f"+ {number}")
+        total = sfn_add(total, number)
+        debug(f"= {total}")
+
+
+    return sfn_mag(total)
+
+def part_two(numbers):
+    return 'n/a'
 
 
 if __name__ == "__main__":
